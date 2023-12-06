8bdec9bce7423f9bb270051c1d21bffd613d4c57
Lucas Dalan <lucas.dalan@frgnet.com.br>
2023-12-06 11:48:57 -0300

    (feat) - validação JWT nos controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,10 +7,12 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create.user.dto';
 import { UpdateUserDto } from './dto/update.user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -23,15 +25,19 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.users();
   }
+
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.user({ id: parseInt(id) });
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update({
       where: { id: parseInt(id) },
@@ -41,6 +47,7 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(204)
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.delete({ id: parseInt(id) });
   }
